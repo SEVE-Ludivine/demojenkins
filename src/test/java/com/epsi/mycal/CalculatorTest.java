30cebb72ea13fc5164df28fe1aae0bb9859c8c96
ludivine <ludivine.seve@epsi.fr>
2021-03-12 08:48:24 +0100

    test multiplication

--- src/test/java/com/epsi/mycal/CalculatorTest.java
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -35,5 +35,14 @@ public class CalculatorTest {
 		int actual = calc.sub(a, b);
 		assertEquals(2, actual);
 	}
+	@Test
+	public void testMul() {
+		int a = 4;
+		int b = 2;
+		
+		Calculator calc = new Calculator();
+		int actual = calc.mul(a, b);
+		assertEquals(8, actual);
+	}
 
 }
