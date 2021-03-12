7f4f7518d070ee0f01cdb309d6d0f82f3d424a97
ludivine <ludivine.seve@epsi.fr>
2021-03-12 13:21:33 +0100

    nouvelle correction buildgradle

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -1,38 +1,45 @@
 package com.epsi.mycal;
-import java.util.Scanner;
-public class Application {
 
+import java.util.Scanner;
+
+public class Application {
+/**
+ *  On lance la calculatrice.
+ * @param args un tableau.
+ */
   public static void main(String[] args) {
     Scanner scanner = new Scanner( System.in );
     Calculator calc = new Calculator();
-    int b = 1;
+    int valueB = 1;
     System.out.print( "Veuillez saisir un calcul (+,-,*,/,prem,pair) : " );
     String calcul = scanner.nextLine();
     System.out.print( "Veuillez saisir une valeur  " );
-    int a = scanner.nextInt();
+    int valueA = scanner.nextInt();
     scanner.nextLine();
     if (!calcul.equalsIgnoreCase("prem") && !calcul.equalsIgnoreCase("pair")) {
       System.out.print( "Veuillez saisir une deuxieme valeur  " );
-      b = scanner.nextInt();
+      valueB = scanner.nextInt();
     }
     switch (calcul) {
       case "*":
-        System.out.println(calc.mul(a,b));
+        System.out.println(calc.mul(valueA,valueB));
         break;
       case "/":
-        System.out.println(calc.div(a,b));
+        System.out.println(calc.div(valueA,valueB));
         break;
       case "-":
-        System.out.println(calc.sub(a,b));
+        System.out.println(calc.sub(valueA,valueB));
         break;
       case "+":
-        System.out.println(calc.add(a,b));
+        System.out.println(calc.add(valueA,valueB));
         break;
       case "prem":
-        System.out.println(calc.isPremier(a));
+        System.out.println(calc.isPremier(valueA));
         break;
       case "pair":
-        System.out.println(calc.nbPair(a));
+        System.out.println(calc.nbPair(valueA));
+        break;
+      default:
         break;
     }
     
--- src/main/java/com/epsi/mycal/Calculator.java
+++ src/main/java/com/epsi/mycal/Calculator.java
@@ -16,23 +16,30 @@ public class Calculator {
   public int div(int valueA, int valueB) {
     return valueA / valueB;
   }
-
+/**
+ * Un commentaire.
+ * @param valueA
+ * @return
+ */
   public boolean nbPair(int valueA) {
     int result = valueA % 2;
     if (result == 1) {
       return false;
-    }
-    else {
+    } else {
       return true;
     }
   }
-  
+  /** 
+   * Un autre commentaire.
+   * @param valueA
+   * @return
+   */
   public boolean isPremier(int valueA) {
     boolean isPremier = true;
     if (valueA < 0) {
       isPremier = false;
     } else if (valueA != 0 && valueA != 1) {
-      for (int i = 2; i <= valueA/2; i++) {
+      for (int i = 2; i <= valueA / 2; i++) {
         if (valueA != i && valueA % i == 0) {
           isPremier = false;
           break;
